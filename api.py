dbe9a90c4d2e5f8d89b2c1a46c0190502991c27e
Coding-Ghostman <atmanmishra1122@gmail.com>
2024-04-26 10:55:10 +0530

    modified:   __pycache__/utils.cpython-310.pyc 	modified:   api.py

--- api.py
+++ api.py
@@ -13,11 +13,14 @@ def upload_file():
     if 'file' not in request.json:
         return jsonify({'error': 'No file part in JSON payload'})
     # Decode the base64 encoded PDF data
-    pdf_data = base64.b64decode(request.json['file'])
-    # Call the extraction function
-    extracted_data = extract_data_from_pdf(pdf_data)
-    print(extracted_data)
-    return extracted_data
+    try:
+        pdf_data = base64.b64decode(request.json['file'])
+        # Call the extraction function
+        extracted_data = extract_data_from_pdf(pdf_data)
+        print(extracted_data)
+        return extracted_data
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
