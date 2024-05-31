01cef4a05aed6cd00e14bb739b48ab10e5b94f3c
Coding-Ghostman <atmanmishra1122@gmail.com>
2024-05-31 18:40:22 +0530

    Refactor currency formatting logic and remove unused code

--- ai_formatter/format_main.py
+++ ai_formatter/format_main.py
@@ -6,7 +6,8 @@ from .llm_process import (
     currency_get_format,
     currency_exchange,
 )
-import oracledb, re
+import oracledb
+import re
 
 
 class formatter:
@@ -89,26 +90,6 @@ def format_changer(
     formatter_data = formatter_.run_query()
     # formatter = [('Jan 05 2023', 'December 2022', 'USD 950,000'), ('02/01/23', 'January 2024', 'USD 38,250')]
     # print(actual_or_estimated)
-    actual_or_estimated_num_list = actual_or_estimated.split()
-    final_actual_or_estimated_list = []
-    for value in actual_or_estimated_num_list:
-        print(value)
-        type_, currency = separate_currency(value)
-        if type_ == "list":
-            final_actual_or_estimated_list.append(currency[0])
-            final_actual_or_estimated_list.append(currency[1])
-        elif type_ == "value":
-            final_actual_or_estimated_list.append(currency)
-        else:
-            final_actual_or_estimated_list.append(value)
-
-    print("hauaaa")
-    print(final_actual_or_estimated_list)
-    actual_or_estimated_num_value = float(
-        [num for num in final_actual_or_estimated_list if is_number(num)][0]
-    )
-    print("finalll")
-    print(actual_or_estimated_num_value)
     data_dict = {
         "requested_delivery_date": [],
         "need_identification_date": [],
@@ -120,55 +101,77 @@ def format_changer(
         data_dict["need_identification_date"].append(i[1])
         data_dict["actual_or_estimated"].append(i[2])
 
-    old_dates_similarity_requested_delivery_dates = date_format_processor(
-        data_dict["requested_delivery_date"][0], data_dict["requested_delivery_date"][1]
-    )
-    old_dates_similarity_need_identification_date = date_format_processor(
-        data_dict["need_identification_date"][0],
-        data_dict["need_identification_date"][1],
-    )
-    old_currency_similarity = currency_format_check(
-        data_dict["actual_or_estimated"][0], data_dict["actual_or_estimated"][1]
-    )
-    if old_dates_similarity_requested_delivery_dates["format"].upper() == "YES":
-        date_similarity_check = date_format_processor(
-            data_dict["requested_delivery_date"][0], requested_delivery_date
+    if requested_delivery_date:
+        old_dates_similarity_requested_delivery_dates = date_format_processor(
+            data_dict["requested_delivery_date"][0],
+            data_dict["requested_delivery_date"][1],
         )
-        if date_similarity_check["format"].upper() == "NO":
-            requested_delivery_date = date_format_convertor(
-                requested_delivery_date, data_dict["requested_delivery_date"][0]
-            )["format"]
-    if old_dates_similarity_need_identification_date["format"].upper() == "YES":
-        date_similarity_check = date_format_processor(
-            data_dict["need_identification_date"][0], need_identification_date
+        if old_dates_similarity_requested_delivery_dates["format"].upper() == "YES":
+            date_similarity_check = date_format_processor(
+                data_dict["requested_delivery_date"][0], requested_delivery_date
+            )
+            if date_similarity_check["format"].upper() == "NO":
+                requested_delivery_date = date_format_convertor(
+                    requested_delivery_date, data_dict["requested_delivery_date"][0]
+                )["format"]
+
+    if need_identification_date:
+        old_dates_similarity_need_identification_date = date_format_processor(
+            data_dict["need_identification_date"][0],
+            data_dict["need_identification_date"][1],
         )
-        if date_similarity_check["format"].upper() == "NO":
-            need_identification_date = date_format_convertor(
-                need_identification_date, data_dict["need_identification_date"][0]
-            )["format"]
-    if old_currency_similarity["same"].upper() == "YES":
-        currency_similarity = currency_format_check(
-            actual_or_estimated, data_dict["actual_or_estimated"][0]
+        if old_dates_similarity_need_identification_date["format"].upper() == "YES":
+            date_similarity_check = date_format_processor(
+                data_dict["need_identification_date"][0], need_identification_date
+            )
+            if date_similarity_check["format"].upper() == "NO":
+                need_identification_date = date_format_convertor(
+                    need_identification_date, data_dict["need_identification_date"][0]
+                )["format"]
+
+    if actual_or_estimated:
+        actual_or_estimated_num_list = actual_or_estimated.split()
+        final_actual_or_estimated_list = []
+        for value in actual_or_estimated_num_list:
+            print(value)
+            type_, currency = separate_currency(value)
+            if type_ == "list":
+                final_actual_or_estimated_list.append(currency[0])
+                final_actual_or_estimated_list.append(currency[1])
+            elif type_ == "value":
+                final_actual_or_estimated_list.append(currency)
+            else:
+                final_actual_or_estimated_list.append(value)
+
+        actual_or_estimated_num_value = float(
+            [num for num in final_actual_or_estimated_list if is_number(num)][0]
         )
-        if currency_similarity["same"].upper() == "NO":
-            actual_or_estimated_dict = currency_get_format(
+        old_currency_similarity = currency_format_check(
+            data_dict["actual_or_estimated"][0], data_dict["actual_or_estimated"][1]
+        )
+        if old_currency_similarity["same"].upper() == "YES":
+            currency_similarity = currency_format_check(
                 actual_or_estimated, data_dict["actual_or_estimated"][0]
             )
-            actual_or_estimated = y = "{:.2f}".format(
-                currency_exchange(
-                    actual_or_estimated_dict["currency1"],
-                    actual_or_estimated_dict["currency2"],
-                    actual_or_estimated_num_value,
+            if currency_similarity["same"].upper() == "NO":
+                actual_or_estimated_dict = currency_get_format(
+                    actual_or_estimated, data_dict["actual_or_estimated"][0]
                 )
-            )
-            final_value = ""
-            format_list = data_dict["actual_or_estimated"][0].split()
-            for i in format_list:
-                if any(char.isdigit() for char in i):
-                    final_value += str(actual_or_estimated) + " "
-                    continue
-                final_value += i + " "
+                actual_or_estimated = y = "{:.2f}".format(
+                    currency_exchange(
+                        actual_or_estimated_dict["currency1"],
+                        actual_or_estimated_dict["currency2"],
+                        actual_or_estimated_num_value,
+                    )
+                )
+                final_value = ""
+                format_list = data_dict["actual_or_estimated"][0].split()
+                for i in format_list:
+                    if any(char.isdigit() for char in i):
+                        final_value += str(actual_or_estimated) + " "
+                        continue
+                    final_value += i + " "
 
-            actual_or_estimated = final_value
+                actual_or_estimated = final_value
 
     return requested_delivery_date, need_identification_date, actual_or_estimated
